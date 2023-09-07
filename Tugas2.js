1c76204504feaff7f8c4f8e8a74a2d63f5267594
Angelina <angelinaylf@gmail.com>
2023-09-07 21:20:07 +0700

    Ini adalah file Tugas JS 2

---
+++ Tugas2.js
@@ -0,0 +1,63 @@
+// Angelina Yulfaningtyas_UDINUS_GRUP 2
+
+// Diketahui: 
+// pegawai: Ridwan 
+// jabatan: manager 
+// status: menikah  
+// Tugas:  
+// Cetak Data Pegawai 
+// Nama Pegawai, Jabatan, Status,  
+// Gaji Pokok(if): Manager=>15 jt, Asisten Manager=>10 jt, Staff=>5 jt  
+// Tunjangan Jabatan: 15% dari gapok 
+// BPJS: 10% dari gapok 
+// Tunjangan Keluarga (ternary): dilihat dari status yg sudah menikah =>20% dari gapok 
+// Total Gaji: jumlahlahkan seluruh element gaji
+
+// Data Karyawan
+let karyawan = {
+    pegawai: "Ridwan",
+    jabatan: "Manager",
+    status: "Menikah",
+};
+
+// Menghitung gaji pokok berdasarkan jabatan
+let gajiPokok;
+switch (karyawan.jabatan) {
+    case "Manager":
+        gajiPokok = 15000000;
+        break;
+
+    case "Asisten Manager":
+        gajiPokok = 10000000;
+        break;
+
+    case "Staf":
+        gajiPokok = 5000000;
+        break;
+
+    default:
+        gajiPokok = 0;
+};
+
+// Tunjangan Jabatan : 15% dari gajipokok
+let tunjanganJabatan = 0.15 * gajiPokok;
+
+// BPJS : 10% dari gajipokok
+let bpjs = 0.10 * gajiPokok;
+
+// Tunjangan keluarga (ternary): dilihat dari status yg sudah menikah =>20% dari gajipokok
+let tunjanganKeluarga = karyawan.status === "Menikah" ? 0.20 * gajiPokok : 0;
+
+// Total Gaji: jumlahlahkan seluruh element gaji
+let totalGaji = gajiPokok + tunjanganJabatan + bpjs + tunjanganKeluarga;
+
+// Menampilkan data pegawai dan total gaji
+console.log("==================Data Pegawai==================");
+console.log("Nama Pegawai:", karyawan.pegawai);
+console.log("Jabatan:", karyawan.jabatan);
+console.log("Status:", karyawan.status);
+console.log("Gaji Pokok:", gajiPokok);
+console.log("Tunjangan jabatan:", tunjanganJabatan);
+console.log("BPJS:", bpjs);
+console.log("Tunjangan Keluarga:", tunjanganKeluarga);
+console.log("TOtal Gaji", totalGaji);
